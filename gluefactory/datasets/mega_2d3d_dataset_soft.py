fix normalization bug and nll balance settings

gluefactory/datasets/mega_2d3d_dataset_soft.py
@@ -173,9 +173,11 @@ class Torch2D3D(Dataset):
             query_feats, p3d_feats, camera, depth_map
         )
         
-        width = camera['intrinsics']['width']
-        height = camera['intrinsics']['height']
-        size0 = torch.tensor([width, height]).float()
+        # width = camera['intrinsics']['width']
+        # height = camera['intrinsics']['height']
+        # size0 = torch.tensor([width, height]).float()
+
+        size0 = torch.from_numpy(query_feats["image_size"]).float()
         size1 = torch.tensor([1.0, 1.0, 1.0]).float()
         
         kpts3d = torch.from_numpy(p3d_feats["keypoints"]).float()
@@ -386,6 +388,7 @@ class Torch2D3D(Dataset):
             query_feature_dict["descriptors"] = ds["descriptors"][:]
             query_feature_dict["scores"] = ds["scores"][:]
             query_feature_dict["keypoints"] = ds["keypoints"][:]
+            query_feature_dict["image_size"] = ds["image_size"][:]
 
         # print(f"Collected descriptors for query image {img_name}.")
         
@@ -431,6 +434,10 @@ class MegaDepth2D3D(BaseDataset):
         samples = []
         for scene in scenes:
             query_path = Path(self.root) / "query_sets" / scene
+            # if split_file == self.conf.split_train:
+            #     query_names_file = query_path / "query_image_names_50_100.txt"
+            # else:
+            #     query_names_file = query_path / "query_image_names_clean.txt"
             query_names_file = query_path / "query_image_names_clean.txt"
             if not query_names_file.exists():
                 continue

gluefactory/models/matchers/lightglu3d_bicross.py
@@ -29,7 +29,8 @@ def normalize_keypoints(
     kpts: torch.Tensor, size: Optional[torch.Tensor] = None
 ) -> torch.Tensor:
     if size is None:
-        size = 1 + kpts.max(-2).values - kpts.min(-2).values
+        # size = 1 + kpts.max(-2).values - kpts.min(-2).values
+        size = kpts.max(-2).values
     elif not isinstance(size, torch.Tensor):
         size = torch.tensor(size, device=kpts.device, dtype=kpts.dtype)
     size = size.to(kpts)
@@ -54,6 +55,24 @@ def normalize_3d_with_quantile(
     kpts_norm = (kpts - shift) / scale * (2 * quantile_value - 1)
 
     return kpts_norm
+
+def apply_y_axis_rotation(kpts3d: torch.Tensor) -> torch.Tensor:
+
+    device = kpts3d.device
+    batch_size = kpts3d.shape[0]
+
+    theta = torch.rand(batch_size, device=device) * 2 * torch.pi # random angle ~ [0, 2*pi]
+    cos_t = torch.cos(theta)
+    sin_t = torch.sin(theta)
+
+    ones = torch.ones_like(cos_t)
+    zeros = torch.zeros_like(cos_t)
+    row1 = torch.stack([cos_t,  zeros, sin_t], dim=-1)
+    row2 = torch.stack([zeros,  ones,  zeros], dim=-1)
+    row3 = torch.stack([-sin_t, zeros, cos_t], dim=-1)
+    R = torch.stack([row1, row2, row3], dim=1) # rotation matrix R of shape (B, 3, 3)
+
+    return torch.bmm(kpts3d, R)
 
 def rotate_half(x: torch.Tensor) -> torch.Tensor:
     x = x.unflatten(-1, (-1, 2))
@@ -394,6 +413,7 @@ class LightGlu3D(nn.Module):
             "fn": "nll",
             "nll_balancing": 0.5,
         },
+        "3d_rotation_aug": False, # apply random y-axis rotation for 3D keypoints during training
     }
 
     required_data_keys = ["keypoints0", "keypoints1", "descriptors0", "descriptors1"]
@@ -494,17 +514,20 @@ class LightGlu3D(nn.Module):
         b, n, _ = kpts1.shape
         mask0 = data.get("mask0", None)
         mask1 = data.get("mask1", None)
+        size0 = data.get("size0", None)
         device = kpts0.device
-        if "view0" in data.keys() and "view1" in data.keys():
+        if "view0" in data.keys():
             size0 = data["view0"].get("image_size")
-            size1 = data["view1"].get("image_size")
         # TODO: Using bounding boxs for now
-        else:
-            size0 = kpts0.max(dim=1).values - kpts0.min(dim=1).values
-            size0 = torch.clamp(size0, min=1e-6)
+        # else:
+        #     size0 = kpts0.max(dim=1).values - kpts0.min(dim=1).values
+        #     size0 = torch.clamp(size0, min=1e-6)
 
         kpts0 = normalize_keypoints(kpts0, size0).clone()
         kpts1 = normalize_3d_with_quantile(kpts1, quantile_value=0.975).clone()
+
+        if self.training and self.conf.get("3d_rotation_aug", False):
+            kpts1 = apply_y_axis_rotation(kpts1)
 
         if self.conf.add_scale_ori:
             sc0, o0 = data["scales0"], data["oris0"]

gluefactory/models/matchers/lightglue_adapt.py
@@ -367,6 +367,24 @@ def filter_matches(scores: torch.Tensor, th: float):
     return m0, m1, mscores0, mscores1
 
 
+class SwiGLUAdaptor(nn.Module):
+    def __init__(self, dim=256, expansion_factor=2):
+        super().__init__()
+        hidden_dim = int(dim * expansion_factor)
+        self.ln = nn.LayerNorm(dim)
+        self.w1 = nn.Linear(dim, hidden_dim)
+        self.w2 = nn.Linear(dim, hidden_dim)
+        self.w3 = nn.Linear(hidden_dim, dim)
+
+    def forward(self, x):
+        # residual + Pre-LN
+        residual = x
+        x = self.ln(x)
+        # x = (w1(x) * SiLU(w2(x))) * w3
+        gate = F.silu(self.w2(x))
+        x = self.w3(self.w1(x) * gate)
+        return x + residual
+    
 class LightGlue(nn.Module):
     default_conf = {
         "name": "lightglue",  # just for interfacing
@@ -413,12 +431,14 @@ class LightGlue(nn.Module):
         h, n, d = conf.num_heads, conf.n_layers, conf.descriptor_dim
 
         # Define the Adapter for 3D features
-        self.adapter_3d = nn.Sequential(
-            nn.Linear(conf.descriptor_dim, conf.descriptor_dim * 2),
-            nn.GELU(),
-            nn.Linear(conf.descriptor_dim * 2, conf.descriptor_dim),
-            nn.LayerNorm(conf.descriptor_dim)
-        )
+        # self.adapter_3d = nn.Sequential(
+        #     nn.Linear(conf.descriptor_dim, conf.descriptor_dim * 2),
+        #     nn.GELU(),
+        #     nn.Linear(conf.descriptor_dim * 2, conf.descriptor_dim),
+        #     nn.LayerNorm(conf.descriptor_dim)
+        # )
+
+        self.adapter_3d = SwiGLUAdaptor(conf.descriptor_dim, expansion_factor=2)
 
         self.transformers = nn.ModuleList(
             [TransformerLayer(d, h, conf.flash) for _ in range(n)]

gluefactory/run_nn_baseline.py
@@ -15,9 +15,9 @@ def run_nn_eval():
             "distance_thresh": 0.75,
         },
         "data": {
-            "name": "mega_2d3d_dataset",
+            "name": "mega_2d3d_dataset_soft",
             "root": "/proj/vlarsson/outputs",
-            "split_val": "splits/val.txt", 
+            "split_val": "splits/test.txt", 
             "batch_size": 1,
             "num_workers": 4,
         }
@@ -49,17 +49,25 @@ def run_nn_eval():
         with torch.no_grad():
             pred = model(data)
 
-        matches = pred['matches0'][0] # [N]
-        gt_matches = data['gt_matches0'][0] # [N]
+        # matches = pred['matches0'][0] # [N]
+        # gt_matches = data['gt_matches0'][0] # [N]
+        matches = pred['matches0']
+        gt_matches = data['gt_matches0']
         
-        correct_matches = (matches == gt_matches) & (gt_matches > -1)
+        mask = ((matches > -1) & (gt_matches >= -1)).float()
+        correct_matches = ((matches == gt_matches) * mask).sum(1)
+        # correct_matches = (matches == gt_matches) & (gt_matches > -1)
         
         num_pred = (matches > -1).sum().item()
-        num_gt = (gt_matches > -1).sum().item()
-        num_correct = correct_matches.sum().item()
+        # num_gt = (gt_matches > -1).sum().item()
+        # num_correct = correct_matches.sum().item()
 
-        precision = num_correct / (1e-8 + num_pred)
-        recall = num_correct / (1e-8 + num_gt)
+        # precision = num_correct / (1e-8 + num_pred)
+        # recall = num_correct / (1e-8 + num_gt)
+
+        recall_mask = (gt_matches > -1).float()
+        precision = correct_matches / (1e-8 + mask.sum(1))
+        recall =  ((matches == gt_matches) * recall_mask).sum(1) / (1e-8 + recall_mask.sum(1))
         
         results["num_matches"].append(num_pred)
         results["precision"].append(precision)